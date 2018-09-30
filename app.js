7c2aaac7db7eb8bb0ab5d1d3445f4093ef6f45db
AntipovAndrey <antipov11.11@gmail.com>
2018-09-30 20:36:47 +0300

    ObjectId validation

--- app.js
+++ app.js
@@ -8,6 +8,15 @@ const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const sanitizer = require('express-sanitizer');
 
+mongoose.plugin(function (schema) {
+    schema.statics.isObjectId = function (id) {
+        if (id) {
+            return /^[0-9a-fA-F]{24}$/.test(id);
+        }
+        return false;
+    };
+});
+
 const blogRouter = require('./routes/blog');
 const indexRouter = require('./routes/index');
 
@@ -41,14 +50,13 @@ app.use(function (req, res, next) {
 
 // error handler
 app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
+    const status = !err.status ? 500 : err.status;
+    res.status(status)
+        .render('error', {
+            message: err.message,
+            error: req.app.get('env') === 'development' ? err : {},
+            status: status
+        });
 });
 
 module.exports = app;
--- routes/blog.js
+++ routes/blog.js
@@ -3,77 +3,91 @@ const router = express.Router();
 const Blog = require('../repository/blog');
 
 
-/* GET home page. */
-router.get('/', (req, res) => {
-    Blog.find({}, (err, blogs) => {
-        if (err) {
-            console.log(err);
-        } else {
+router.get('/', (req, res, next) => {
+    Blog.find({})
+        .then(blogs => {
             res.render('index', {
                 blogs: blogs
             });
-        }
-    });
+        })
+        .catch(next);
+
 });
 
 router.get('/new', (req, res) => {
     res.render('new');
 });
 
-router.post('/', (req, res) => {
+router.post('/', (req, res, next) => {
     req.body.blog.body = req.sanitize(req.body.blog.body);
-    Blog.create(req.body.blog, (err, newBlog) => {
-        if (err) {
-            res.redirect(req.baseUrl + '/new')
-        } else {
+    Blog.create(req.body.blog)
+        .then(newBlog => {
             res.redirect(`${req.baseUrl}/${newBlog._id}`);
-        }
-    })
+        })
+        .catch(next)
 });
 
-router.get('/:id', (req, res) => {
-    Blog.findById(req.params.id, (err, blog) => {
-        if (err) {
-            res.redirect(req.baseUrl + '/');
-        } else {
+router.get('/:id', (req, res, next) => {
+    if (!Blog.isObjectId(req.params.id)) {
+        next();
+        return;
+    }
+    Blog.findById(req.params.id)
+        .then(blog => {
+            if (!blog) {
+                next();
+                return;
+            }
             res.render('show', {
                 blog: blog
             });
-        }
-    })
+        })
+        .catch(next);
 });
 
-router.get('/:id/edit', (req, res) => {
-    Blog.findById(req.params.id, (err, blog) => {
-        if (err) {
-            res.redirect(`${req.baseUrl}/${req.params.id}/edit`);
-        } else {
+router.get('/:id/edit', (req, res, next) => {
+    if (!Blog.isObjectId(req.params.id)) {
+        next();
+        return;
+    }
+    Blog.findById(req.params.id)
+        .then(blog => {
+            if (!blog) {
+                next();
+                return;
+            }
             res.render('edit', {
                 blog: blog
             });
-        }
-    })
+        })
+        .catch(next);
 });
 
-router.put('/:id/edit', (req, res) => {
+router.put('/:id/edit', (req, res, next) => {
+    if (!Blog.isObjectId(req.params.id)) {
+        next();
+        return;
+    }
     req.body.blog.body = req.sanitize(req.body.blog.body);
-    Blog.findByIdAndUpdate(req.params.id, req.body.blog, (err, updatedBlog) => {
-        if (err) {
-            res.redirect('/');
-        } else {
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog)
+        .then(updatedBlog => {
+            if (!updatedBlog) {
+                next();
+                return;
+            }
             res.redirect(`${req.baseUrl}/${updatedBlog._id}`);
-        }
-    });
+        })
+        .catch(next);
 });
 
-router.delete('/:id', (req, res) => {
-    Blog.findByIdAndDelete(req.params.id, (err) => {
-        if (err) {
-            res.redirect(req.baseUrl + '/');
-        } else {
-            res.redirect(req.baseUrl + '/');
-        }
-    });
+router.delete('/:id', (req, res, next) => {
+    if (!Blog.isObjectId(req.params.id)) {
+        next();
+        return;
+    }
+    Blog.findByIdAndDelete(req.params.id)
+        .then(() => res.redirect(req.baseUrl + '/'))
+        .catch(next);
 });
 
 module.exports = router;
